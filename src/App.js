0b863d21b55ce7423ce70613d6de5c4b1bdc06eb
Sarbjeet <97713973+sarbjeet7@users.noreply.github.com>
2022-07-07 17:27:59 -0400

    adding the google  authenctication functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login';
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const client_id="535034517954-brm7ca15fra50u5qkn1rt5i4i7o3s0gt.apps.googleusercontent.com";
+
+
 
 function App() {
+  useEffect (()=>{
+    function start(){
+      gapi.client.init({
+        client_id: client_id,
+        scope:""
+      })
+    }
+   
+
+    gapi.load("clinet:auth2", start);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
+     
     </div>
   );
 }
---
+++ src/components/login.js
@@ -0,0 +1,39 @@
+import { GoogleLogin }  from 'react-google-login';
+
+const client_id="535034517954-brm7ca15fra50u5qkn1rt5i4i7o3s0gt.apps.googleusercontent.com";
+
+function Login()
+{
+    const onSuccess=(res)=> 
+    {
+        console.log("Login Successfully");
+    }
+
+   const onFailure=(res)=> 
+   {
+         console.log("Login Failed");
+   }
+
+
+    return (
+      <div id="SignInBtn">
+         <GoogleLogin
+           clientID={client_id}
+           buttonText="Login"
+           onSuccess={onSuccess}
+           onFailure={onFailure}     
+           cookiePolicy={'single_host_origin'}
+           isSignedIn={true}    
+           />
+
+
+
+         </div>
+
+         
+    )
+}
+
+export default Login
+
+
